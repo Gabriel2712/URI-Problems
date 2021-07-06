40d2596d7258c18b50faf03d9361166610e4eea4
Gabriel2712 <gabrielcabral952@gmail.com>
2021-07-05 23:23:34 -0300

    Adicionando primeiros exs em python

---
+++ Python/1001.py
@@ -0,0 +1,12 @@
+A = int(input())
+B = int(input())
+
+X = A + B
+print('X = {}'.format(X))
+print('X =', X, '\n')
+
+"""x = x.replace('R$', '') # remove o R$
+x = x.strip()           # remove espaços em branco
+x = x.replace('.', '')  # remove os pontos
+x = x.replace(',', '.') # troca  virgula do decimal por ponto"""
+# as linhas acima funcionam para quando eu quiser mudar a separação de casa decimal
