866fa372d2cbffac2c2d447b5b1a67dc3e29f03c
Gabriel2712 <gabrielcabral952@gmail.com>
2021-07-06 23:11:58 -0300

    adicionando exs 1006, 1007 e 1008

---
+++ Python/1006.py
@@ -0,0 +1,10 @@
+A = input()
+A = A[0:3]
+B = input()
+B = B[0:3]
+C = input()
+C = C[0:3]
+
+media = float(A) * 0.2 + float(B) * 0.3 + float(C) * 0.5
+
+print('MEDIA = {:.1f}'.format(media))
---
+++ Python/1008.py
@@ -0,0 +1,7 @@
+numero = int(input())
+horas = int(input())
+valor = float(input())
+
+salario = horas * valor
+
+print('NUMBER = {}\nSALARY = U$ {:.2f}'.format(numero, salario))
